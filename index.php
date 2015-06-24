0fc9ba16dd818c31faab2ad42a39451f8a9801b9
zyzo <danghaian168@gmail.com>
2015-06-24 12:48:44 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+Welcome to timeline.api.fossasia.net ! <br/>
+
+The project's source code can be found at <a href="https://github.com/fossasia/timeline.api.fossasia.net">fossasia/timeline.api.fossasia.net</a> on Github.
